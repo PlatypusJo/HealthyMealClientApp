ca8f19475baff78ccb411b56fe5a823205453988
Muravyov Maxim <maxim_muravyov@mail.ru>
2024-04-16 14:22:46 +0300

    Did all main pages

---
+++ HealthyMeal/ViewModels/RecipesPageViewModel.cs
@@ -0,0 +1,126 @@
+﻿using HealthyMeal.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace HealthyMeal.ViewModels
+{
+    public class RecipesPageViewModel : BaseViewModel
+    {
+        #region Поля
+
+        private int _pageIndex = 1;
+
+        private bool _isVisible = true;
+
+        private bool _isVisibleToNext;
+
+        private bool _isVisibleToPrevious;
+
+        ObservableCollection<RecipeModel> _recipes;
+
+        #endregion
+
+        #region Свойства
+
+        public int PageIndex
+        {
+            get => _pageIndex;
+            set
+            {
+                _pageIndex = value;
+                NotifyPropertyChanged(nameof(PageIndex));
+            }
+        }
+        
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                NotifyPropertyChanged(nameof(IsVisible));
+            }
+        }
+        
+        public bool IsVisibleToNext
+        {
+            get => _isVisibleToNext;
+            set
+            {
+                _isVisibleToNext = value;
+                NotifyPropertyChanged(nameof(IsVisibleToNext));
+            }
+        }
+        
+        public bool IsVisibleToPrevious
+        {
+            get => _isVisibleToPrevious;
+            set
+            {
+                _isVisibleToPrevious = value;
+                NotifyPropertyChanged(nameof(IsVisibleToPrevious));
+            }
+        }
+
+        public ObservableCollection<RecipeModel> Recipes
+        {
+            get => _recipes;
+        }
+
+        #endregion
+
+        #region Команды
+
+        public ICommand NextPageCommand { get; private set; }
+
+        public ICommand BackPageCommand { get; private set; }
+
+        #endregion
+
+        #region Конструктор
+
+        public RecipesPageViewModel()
+        {
+            LoadRecipes();
+            _isVisible = _recipes.Count > 0;
+            _isVisibleToNext = true; 
+            _isVisibleToPrevious = true;
+        }
+
+        #endregion
+
+        #region Внутренние методы
+
+        private void LoadRecipes()
+        {
+            _recipes = new ObservableCollection<RecipeModel>()
+            {
+                new RecipeModel()
+                {
+                    Name = "Овощной суп",
+                    Id = 1,
+                },
+                new RecipeModel()
+                {
+                    Name = "Борщ",
+                    Id = 2,
+                },
+                new RecipeModel()
+                {
+                    Name = "Фруктовый салат",
+                    Id = 3,
+                },
+                new RecipeModel()
+                {
+                    Name = "Греческий салат",
+                    Id = 4,
+                },
+            };
+        }
+
+        #endregion
+    }
+}
--- HealthyMeal/Views/RecipesPage.xaml.cs
+++ HealthyMeal/Views/RecipesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthyMeal.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace HealthyMeal.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecipesPage : ContentPage
     {
+        private RecipesPageViewModel _vm;
         public RecipesPage()
         {
             InitializeComponent();
+            BindingContext = _vm = new RecipesPageViewModel();
         }
     }
 }

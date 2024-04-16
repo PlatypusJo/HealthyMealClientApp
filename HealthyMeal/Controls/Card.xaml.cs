a0c0c80c5d437ef3c6096f71df148711ed28383f
Muravyov Maxim <maxim_muravyov@mail.ru>
2024-04-16 11:48:10 +0300

    Added default Card control

---
+++ HealthyMeal/Controls/Card.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HealthyMeal.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Card : ContentView
+    {
+        public static readonly BindableProperty PhotoProperty = BindableProperty.Create(
+            nameof(Photo),
+            typeof(ImageSource),
+            typeof(Card),
+            null);
+        public static readonly BindableProperty NameProperty = BindableProperty.Create(
+            nameof(Name),
+            typeof(string),
+            typeof(Card),
+            string.Empty);
+        public static readonly BindableProperty KcalValueProperty = BindableProperty.Create(
+            nameof(KcalValue),
+            typeof(string),
+            typeof(Card),
+            string.Empty);
+        public static readonly BindableProperty CookingTimeProperty = BindableProperty.Create(
+            nameof(CookingTime),
+            typeof(string),
+            typeof(Card),
+            string.Empty);
+        public static readonly BindableProperty TapCommandProperty = BindableProperty.Create(
+            nameof(TapCommand),
+            typeof(Command),
+            typeof(Card),
+            null);
+        public static readonly BindableProperty TapCommandParameterProperty = BindableProperty.Create(
+            nameof(TapCommandParameter),
+            typeof(object),
+            typeof(Card),
+            null);
+
+        public ImageSource Photo
+        {
+            get => (ImageSource)GetValue(PhotoProperty);
+            set => SetValue(PhotoProperty, value);
+        }
+        public string Name
+        {
+            get => (string)GetValue(NameProperty);
+            set => SetValue(NameProperty, value);
+        }
+        public string KcalValue
+        {
+            get => (string)GetValue(KcalValueProperty);
+            set => SetValue(KcalValueProperty, value);
+        }
+        public string CookingTime
+        {
+            get => (string)GetValue(CookingTimeProperty);
+            set => SetValue(CookingTimeProperty, value);
+        }
+        public Command TapCommand
+        {
+            get => (Command)GetValue(TapCommandProperty);
+            set => SetValue(TapCommandProperty, value);
+        }
+        public object TapCommandParameter
+        {
+            get => (object)GetValue(TapCommandParameterProperty);
+            set => SetValue(TapCommandParameterProperty, value);
+        }
+
+        public Card()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HealthyMeal/Controls/CardWithAction.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HealthyMeal.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardWithAction : ContentView
+    {
+        public CardWithAction()
+        {
+            InitializeComponent();
+        }
+    }
+}

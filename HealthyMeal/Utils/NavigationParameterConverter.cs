d7e77e574e27a8935d1bfd06602afdcf41c4f1ae
Muravyov Maxim <maxim_muravyov@mail.ru>
2024-04-19 00:52:53 +0300

    Refactoring

---
+++ HealthyMeal/Utils/NavigationParameterConverter.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace HealthyMeal.Utils
+{
+    public static class NavigationParameterConverter
+    {
+        public static string ObjectToPairKeyValue(object obj, string objectName)
+        {
+            if (obj != null)
+                return $"{objectName}={JsonConvert.SerializeObject(obj)}";
+            return null;
+        }
+
+        public static T ObjectFromPairKeyValue<T>(string parameter)
+        {
+            if (string.IsNullOrEmpty(parameter))
+                throw new ArgumentNullException("Пустой параметр");
+            return JsonConvert.DeserializeObject<T>(parameter);
+        }
+    }
+}
